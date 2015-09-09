64428cd7d859aa54f591afca9b786f51e3237bac
Yannyezixin <646840080@qq.com>
2015-09-09 23:53:34 +0800

    update uniq keep order list

--- stack-qa/list/uniq_keep_order.py
+++ stack-qa/list/uniq_keep_order.py
@@ -1,5 +1,7 @@
 
 def uniq(input):
+    # order preserving
+    # O(n^2)
     output = []
     for i in input:
         if i not in output:
@@ -8,13 +10,52 @@ def uniq(input):
     return output
 
 def uniq_q(seq):
+    # order preserving
+    # O(n^2)
     seen = set()
     seen_add = seen.add
     return [x for x in seq if x not in seen and not seen_add(x)]
 
+def uniq_3(seq):
+    # not order preserving
+    # O(n)
+    keys = {}
+    for e in seq:
+        keys[e] = 1
+    return keys.keys()
 
-old_list = [1, 2, 3, 3, 4, 5, 6, 3]
+def uniq_4(seq):
+    # not order preserving
+    # O(n)
+    Set =  set(seq)
+    return list(Set)
+
+def uniq_5(seq):
+    # order preserving
+    # O(n^2)
+    output = []
+    [output.append(i) for i in seq if not output.count(i)]
+    return output
+
+def uniq_6(seq):
+    # order preserving
+    # Best way
+    # O(n)
+    seen = {}
+    output = []
+    for item in seq:
+        if item in seen: continue
+        seen[item] = 1
+        output.append(item)
+    return output
+
+
+old_list = [10, 1, 2, 3, 3, 4, 5, 6, 3, 6, 9]
 
 print uniq_q(old_list)
 print uniq(old_list)
+print uniq_3(old_list)
+print uniq_4(old_list)
+print uniq_5(old_list)
+print uniq_6(old_list)
 
